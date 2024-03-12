5e5487f5d0ccfdc49cb122e27fe71e1175911935
Willenborx <holger@willenb.org>
2024-03-12 22:59:39 +0100

    ReworkUpdate Version info after rework

--- include/LoudnessBasePico.hpp
+++ include/LoudnessBasePico.hpp
@@ -3,10 +3,10 @@
  * @author Holger Willenborg (holger@willenb.org)
  * @brief Implementation of LoudnessBase for Pico
  * 
- * @version 0.5
- * @date 2023-11-01
+ * @version 0.6
+ * @date 2024-03-12
  * 
- * @copyright Copyright (c) 2023
+ * @copyright Copyright (c) 2024
  * 
  */
 
